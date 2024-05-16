a2285a02a3fc63666177cc52737ed77c757431f1
fai7h01 <l.varsimashvil10@gmail.com>
2024-05-16 15:45:31 +0400

    initial commit

---
+++ src/main/java/com/cydeo/converter/RoleDTOConverter.java
@@ -0,0 +1,26 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class RoleDTOConverter implements Converter<String, RoleDTO> {
+
+    private final RoleService roleService;
+
+    public RoleDTOConverter(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Override
+    public RoleDTO convert(String source) {
+        if (source == null || source.isEmpty()){
+            return null;
+        }
+        return roleService.findById(Long.parseLong(source));
+    }
+}
---
+++ src/main/java/com/cydeo/converter/UserDTOConverter.java
@@ -0,0 +1,26 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class UserDTOConverter implements Converter<String, UserDTO> {
+
+    private final UserService userService;
+
+    public UserDTOConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+        if (source == null || source.isEmpty()){
+            return null;
+        }
+        return userService.findById(source);
+    }
+}
